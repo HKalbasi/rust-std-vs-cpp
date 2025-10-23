d02438c82579db9dde80be1416eeb67bebe8eff1
MahdiGMK <mbahramiand@gmail.com>
2025-10-23 15:12:28 +0330

    add zig-treap and compile/bench command

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 mkdir -p bin
 clang++ -std=c++26 benches/$1.cpp -O3 -o bin/cpp-$1
 rustc benches/$1.rs -O -o bin/rust-$1
+zig build-exe benches/$1.zig -O ReleaseFast --name zig-$1 && mv zig-$1 bin/zig-$1
